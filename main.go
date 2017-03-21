47a39eb3929467925eff3c1dae42447a55b0fa6e
b4b4r07 <b4b4r07@gmail.com>
2017-03-21 16:55:47 +0900

    Replace api client with github.com/b4b4r07/go-crowi

--- main.go
+++ main.go
@@ -2,13 +2,75 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"path"
 	"regexp"
 	"strings"
 	"sync"
+	"time"
+
+	"github.com/b4b4r07/go-crowi"
+)
+
+type Qiita struct {
+	Articles []Articles    `json:"articles"`
+	Groups   []interface{} `json:"groups"`
+	Projects []Projects    `json:"projects"`
+	Version  string        `json:"version"`
+}
+
+type Articles struct {
+	RenderedBody string      `json:"rendered_body"`
+	Body         string      `json:"body"`
+	Coediting    bool        `json:"coediting"`
+	CreatedAt    time.Time   `json:"created_at"`
+	Group        interface{} `json:"group"`
+	ID           string      `json:"id"`
+	Private      bool        `json:"private"`
+	Tags         []struct {
+		Name     string        `json:"name"`
+		Versions []interface{} `json:"versions"`
+	} `json:"tags"`
+	Title     string    `json:"title"`
+	UpdatedAt time.Time `json:"updated_at"`
+	URL       string    `json:"url"`
+	User      struct {
+		ID              string `json:"id"`
+		PermanentID     int    `json:"permanent_id"`
+		ProfileImageURL string `json:"profile_image_url"`
+	} `json:"user"`
+	Comments []interface{} `json:"comments"`
+}
+
+type Projects struct {
+	RenderedBody string    `json:"rendered_body"`
+	Archived     bool      `json:"archived"`
+	Body         string    `json:"body"`
+	CreatedAt    time.Time `json:"created_at"`
+	ID           int       `json:"id"`
+	Name         string    `json:"name"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	User         struct {
+		ID              string `json:"id"`
+		PermanentID     int    `json:"permanent_id"`
+		ProfileImageURL string `json:"profile_image_url"`
+	} `json:"user"`
+}
+
+var urlSafe = strings.NewReplacer(
+	`^`, `＾`, // for Crowi's regexp
+	`$`, `＄`,
+	`*`, `＊`,
+	`%`, `％`, // query
+	`?`, `？`,
+	`/`, `／`, // Prevent unexpected stratification
 )
 
 var (
@@ -35,10 +97,16 @@ func main() {
 	wg := sync.WaitGroup{}
 	errs := 0
 
+	client, err := crowi.NewClient(*crowiUrl, *accessToken)
+	if err != nil {
+		log.Printf("[ERROR] %s", err.Error())
+		os.Exit(1)
+	}
+
 	for _, article := range q.Articles {
 		wg.Add(1)
 		go func(a Articles) {
-			err := qiita2crowi(a)
+			err := qiita2crowi(client, a)
 			if err != nil {
 				log.Printf("[ERROR] %s", err.Error())
 				errs++
@@ -54,26 +122,30 @@ func main() {
 	}
 }
 
-func qiita2crowi(article Articles) error {
+func qiita2crowi(client *crowi.Client, article Articles) error {
 	ch <- 1
 	defer func() { <-ch }()
 
+	pagePath := getTitlePath(*pagePath, article.Title)
+	if !path.IsAbs(pagePath) {
+		return fmt.Errorf("%s: invalid page path", pagePath)
+	}
 	// Create Crowi page
 	article.Body = fmt.Sprintf("<!-- Imported by\n%s\n-->\n\n%s",
 		strings.TrimLeft(article.URL, "https://"),
 		article.Body,
 	)
-	crowi, err := crowiPageCreate(article.Title, article.Body)
+	res, err := client.PagesCreate(pagePath, article.Body)
 	if err != nil {
 		return err
 	}
-	if !crowi.OK {
-		return fmt.Errorf("Failed to create Crowi page: %s (%s)", article.Title, crowi.Error)
+	if !res.OK {
+		return errors.New(res.Error)
 	}
 
 	// Download images in the Qiita text
 	// then upload to Crowi
-	pageId := crowi.Page.ID
+	pageId := res.Page.ID
 	body := article.Body
 	matched := img.FindAllStringSubmatch(article.RenderedBody, -1)
 	if len(matched) > 0 {
@@ -83,17 +155,23 @@ func qiita2crowi(article Articles) error {
 				if err != nil {
 					return err
 				}
-				crowi, err := crowiAttachmentsAdd(pageId, file)
+				res, err := client.AttachmentsAdd(pageId, file)
 				if err != nil {
 					return err
 				}
-				body = strings.Replace(body, urls[i], crowi.Filename, -1)
+				if !res.OK {
+					return errors.New(res.Error)
+				}
+				body = strings.Replace(body, urls[i], res.Filename, -1)
 			}
 		}
 		// Update image's links in the Crowi page
-		_, err := crowiPageUpdate(pageId, body)
+		res, err = client.PagesUpdate(pageId, body)
 		if err != nil {
 			return err
+		}
+		if !res.OK {
+			return errors.New(res.Error)
 		}
 	}
 
@@ -104,11 +182,64 @@ func qiita2crowi(article Articles) error {
 			body += fmt.Sprintf("## %s\n", comment.(map[string]interface{})["user"].(map[string]interface{})["id"].(string))
 			body += comment.(map[string]interface{})["body"].(string)
 		}
-		_, err = crowiPageUpdate(pageId, body)
+		res, err = client.PagesUpdate(pageId, body)
 		if err != nil {
 			return err
+		}
+		if !res.OK {
+			return errors.New(res.Error)
 		}
 	}
 
 	return nil
+}
+
+func getApiPath(baseUri, endPoint string) (string, error) {
+	base, err := url.Parse(baseUri)
+	if err != nil {
+		return "", err
+	}
+	ep, err := url.Parse(endPoint)
+	if err != nil {
+		return "", err
+	}
+	return base.ResolveReference(ep).String(), nil
+}
+
+func getSaftyPath(path string) string {
+	return urlSafe.Replace(path)
+}
+
+func getTitlePath(defaultPath, titlePath string) string {
+	return path.Clean(path.Join(
+		defaultPath,
+		getSaftyPath(titlePath),
+	))
+}
+
+func downloadImage(url string) (string, error) {
+	filename := ""
+	response, err := http.Get(url)
+	if err != nil {
+		return filename, err
+	}
+
+	// response.Status
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return filename, err
+	}
+
+	_, filename = path.Split(url)
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return filename, err
+	}
+
+	defer func() {
+		file.Close()
+	}()
+
+	file.Write(body)
+	return filename, nil
 }
